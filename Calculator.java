59a37f0693fe967294896ed259e4f29ef9bf598e
Nonamus <yakhzhik@gmail.com>
2018-09-12 21:28:39 +0300

    adding files with code

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    private double x, y;
+
+    public Calculator(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double Plus() {
+        return x+y;
+    }
+
+    public double Multiply() {
+        return x*y;
+    }
+
+    public double Divide() {
+        return x/y;
+    }
+
+    public double Minus() {
+        return x-y;
+    }
+
+}
---
+++ Starter.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        double x, y;
+        Scanner scan=new Scanner(System.in);
+        String operation;
+
+        System.out.println("Input operation:");
+        operation=scan.next();
+
+        System.out.println("Input first number:");
+        x=scan.nextDouble();
+
+        System.out.println("Input second number:");
+        y=scan.nextDouble();
+
+        Calculator calculator_Object=new Calculator(x,y);
+
+        switch (operation) {
+            case "+":
+                System.out.println(operation + ": " + calculator_Object.Plus());
+                break;
+            case "*":
+                System.out.println(operation + ": " + calculator_Object.Multiply());
+                break;
+            case "-":
+                System.out.println(operation + ": " + calculator_Object.Minus());
+                break;
+            case "/":
+                System.out.println(operation + ": " + calculator_Object.Divide());
+                break;
+                default:
+                    System.out.println(operation + " is wrong operation");
+                    break;
+
+        }
+    }
+}
